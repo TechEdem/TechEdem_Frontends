3b409c9a3ef1b7509f09854423bb7ad29f530472
TechEdem <edemaheto3@gmail.com>
2023-06-16 09:15:31 -0400

    Contact

--- Jesste_Frontend_React/src/components/ContactInroduction.jsx
+++ Jesste_Frontend_React/src/components/ContactInroduction.jsx
@@ -33,48 +33,15 @@ function ContactInroduction() {
         <div className='introduction'>
             <video src ={Construction} autoPlay loop muted/>
             <div className='description'>
-                <Navbar />
-                    <div className='contact_details'>
-
-                            <Typed strings={["Contact Us",]} typeSpeed = {80} backSpeed={80} loop  className='typed'/>
-
-                        <div className='main_info'>
-
-                            <div className='main_contact_info'>
-                                {
-                                    info.map((each, index)=>(
-                                        <div className='contact_info' key={index}>
-                                            <img src={each.image} alt= 'logo' />
-                                            <h4>{each.title}</h4>
-                                            <p>{each.description}</p>
-                                        </div>
-                                    ))
-                                }
-                            </div>
-
-                            <form>
-                                <div className='input_box' >
-                                    <div className='main_input'>
-                                        <input type='fullname' name='fullname' placeholder= 'Enter your fullname' className='input'/>
-                                    </div>
-                                    <br />
-                                    <div className='main_input'>
-                                        <input type='email' name='email' placeholder= 'Enter your email' className='input'/>
-                                    </div>
-                                    <br />
-                                    <div className='main_input'>
-                                        <textarea placeholder= 'Enter your message' className='textarea' />
-                                    </div>
-                                    <div className='main_inpu'>
-                                        <button>Submit</button>
-                                    </div>
-                                </div>
-                                
-                            </form>
-                        </div>
-                </div>
-            </div>
-            
+              <Navbar />
+              <div className='description-text'>
+                <Typed strings={["Contact Us",]} typeSpeed = {80} backSpeed={80} loop  className='title'/>
+                <p>
+                In id enim odio. Nunc aliquet diam tortor, at venenatis urna sagittis non. Suspendisse sodales nulla sit amet sem 
+                condimentum, ac euismod nibh elementum. Praesent eu urna at sem sodales venenatis. Mauris efficitur dapibus enim in posuere
+                </p>
+              </div>
+            </div>    
         </div>
     </Container>
   )
@@ -83,90 +50,36 @@ function ContactInroduction() {
 const Container = styled.div`
 .introduction {
     width: 100%;
-    height: 100vh; 
+    height: 80vh; 
     display: flex;
   }
   video {
     width: 100%;
-    height: 100vh;
+    height: 80vh;
     object-fit: cover;
   }
   .description{
     position: absolute;
     width: 100%;
-    height: 100%;
-    background-color:  rgba(0, 0, 0, 0.64);
+    height: 80%;
+    background-color:  rgba(0, 0, 0, 0.44);
   }
-  .typed{
-    text-align: center;
+  .description-text{
+    justify-content: center;
+    color: white;
+    padding: 4% 15%;
+  }
+  .title{
+    padding-left: 30%;
     font-size: 70px;
-    font-family: 'Kanit';
-    padding-left: 40%;
-    color: #FFE6C8;
+    font-family: 'Poppins';
+    font-weight: 700;
   }
-  .main_info{
-    display: flex;
-    padding: 2% 5%;
-    weight: 100%;
+  .description-text p{
+    padding: 1% 20%;
+    font-family: 'Rubik';
+    line-height: 30px;
   }
-  .main_contact_info{
-    border: 3px solid white;
-    padding: 2% 5%;
-    width: 33%;
-    border-radius: 20px;
-    background: rgba(89, 89, 89, 0.71);
-  }
-  .contact_info{
-    display: flex;
-    color: white;
-    padding: 6% 10%;
-  }
-  .contact_info h4{
-    padding-top: 0.5%;
-    padding-left: 2%;
-  }
-  .contact_info p{
-    padding-top: 0.5%;
-    padding-left: 4%;
-  }
-  form{
-    padding: 0% 10%;
-  }
-  .input{
-    width: 500px;
-    padding: 3%;
-    border: none;
-    border-radius: 10px;
-    outline: none;
-    height: 4vh;
-  }
-  .textarea{
-    width: 500px;
-    padding: 3%;
-    border: none;
-    border-radius: 10px;
-    resize: none;
-    outline: none;
-    height: 15vh;
-  }
-  button{
-    width: 300px;
-    height: 8vh;
-    border-radius: 10px;
-    border: 2px solid white;
-    font-family: 'Kanit';
-    font-size: 20px;
-    background: transparent;
-    color: white;
-  }
-  @media (max-width: 900px){
-    .introduction{
-      height: 105vh;
-    }
-    .description{
-      width: 100.50%;
-      height: 105vh;
-    }
-  }
+
 `
 export default ContactInroduction
